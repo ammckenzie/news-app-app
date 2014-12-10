d4b9b73281618af10f156a23eb9b57e2099abd67
Matthew White <mwhite7@lesley.edu>
2014-12-09 20:59:26 -0500

    cleaning up a bunch
    Only need one declaration of document.ready and other consolidation

--- news-app/platforms/ios/www/js/scripts.js
+++ news-app/platforms/ios/www/js/scripts.js
@@ -1,7 +1,65 @@
+// IOS SPECIFIC
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+function onLoad() {
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// Cordova is loaded and it is now safe to make calls Cordova methods
+function onDeviceReady() {
+    console.log("onDeviceReady");
+}
+
+// Handle the online event
+function onOnline() {
+    console.log("onOnline");
+}
+function onOffline() {
+    console.log("onOffline");
+}
+
+
+
+
+// GET FEEDS - PASS to feeds.html
 function myCall() {
         var request = $.ajax({
             // url: "http://localhost:8888/news-app-app/test.php",
-             url: "http://localhost:8888/news-app-app/rsstest.php",
+            url: "http://localhost:8888/news-app-app/rsstest.php",
             type: "GET",            
             dataType: "html"
         });
@@ -18,94 +76,23 @@ function myCall() {
 
 
 
-// function openURL(urlString){
-//     myURL = encodeURI(urlString);
-//     window.open(myURL, '_blank');
-// }
 
+
+// All Other Stuff that fires when the page loads
 $(document).ready(function() {
-
-
 
 		/* Show Menu Tags */
         $('#showmenutags').click(function() {
-
-                $('.menutags').slideToggle("fast");
-                $('.tags-add').toggleClass('tags-minus');
-
+            $('.menutags').slideToggle("fast");
+            $('.tags-add').toggleClass('tags-minus');
         });
 
         /* Show Menu Sites */
-           $('#showmenusites').click(function() {
-
+        $('#showmenusites').click(function() {
                 $('.menusites').slideToggle("fast");
                 $('.sites-add').toggleClass('sites-minus');
-
-
         });
 
-        
-
-
-
-
-        $('.flyout').click(function() {
-        	$('.grayout').fadeIn();
-        	$('.menu').animate({left:'100%'});
-        	$('#site').css('position','fixed');
-        });
-
-        $('.flyout-done').click(function() {
-        	$('.grayout').fadeOut();
-        	$('.menu').animate({left:'-100%'});
-        	$('.menutags').slideUp();
-        	$('.menusites').slideUp();
-        	$('.article-container').css('position','relative');
-            $('.sites-add').removeClass('sites-minus');
-            $('.tags-add').removeClass('tags-minus');
-       });
-
-
-
-
-        /* Search Bar JS */
-
-        function hoverClickLeft() {
-            $('#slide-left').removeClass('slide-left-secondary');
-            if ($('#slide-left').hasClass('slid-left')) {} else {
-                $('#slide-left').addClass("slid-left");
-                $('input[name=search1]').show();
-                $('input[name=search1]').focus();
-            }
-            if ($('#slide-right').hasClass('slid-right')) {
-                $('#slide-right').removeClass('slid-right');
-                $('input[name=search2]').hide();
-            } else {}
-            $('#search-text').addClass('search-transition');
-    };
-
-      // $('.searchslide').click(function() {
-      //           $('.title').hide()
-      //   });
-
-
-
-    function hoverClickRight() {
-        if ($('#slide-right').hasClass('slid-right')) {} else {
-            $('#slide-right').addClass("slid-right");
-            $('input[name=search2]').show();
-            $('input[name=search2]').focus();
-        }
-        if ($('#slide-left').hasClass('slid-left')) {
-            $('#slide-left').removeClass('slid-left');
-            $('input[name=search1]').hide();
-        } else {}
-        $('#slide-left').addClass('slide-left-secondary');
-        $('#search-text').addClass('search-transition');
-    };
-
-
-    $(document).ready(function () {
         $('input[name=search1]').hide();
         $('input[name=search2]').hide();
         $('#slide-left').on('click', function () {
@@ -128,11 +115,50 @@ $(document).ready(function() {
             }
         });
 
-    });
+        $('.flyout').click(function() {
+        	$('.grayout').fadeIn();
+        	$('.menu').animate({left:'100%'});
+        	$('#site').css('position','fixed');
+        });
+
+        $('.flyout-done').click(function() {
+        	$('.grayout').fadeOut();
+        	$('.menu').animate({left:'-100%'});
+        	$('.menutags').slideUp();
+        	$('.menusites').slideUp();
+        	$('.article-container').css('position','relative');
+            $('.sites-add').removeClass('sites-minus');
+            $('.tags-add').removeClass('tags-minus');
+       });
 
 
+        /* Search Bar JS */
 
+    function hoverClickLeft() {
+            $('#slide-left').removeClass('slide-left-secondary');
+            if ($('#slide-left').hasClass('slid-left')) {} else {
+                $('#slide-left').addClass("slid-left");
+                $('input[name=search1]').show();
+                $('input[name=search1]').focus();
+            }
+            if ($('#slide-right').hasClass('slid-right')) {
+                $('#slide-right').removeClass('slid-right');
+                $('input[name=search2]').hide();
+            } else {}
+            $('#search-text').addClass('search-transition');
+    };
 
-        
-
-    });
+    function hoverClickRight() {
+            if ($('#slide-right').hasClass('slid-right')) {} else {
+                $('#slide-right').addClass("slid-right");
+                $('input[name=search2]').show();
+                $('input[name=search2]').focus();
+            }
+            if ($('#slide-left').hasClass('slid-left')) {
+                $('#slide-left').removeClass('slid-left');
+                $('input[name=search1]').hide();
+            } else {}
+            $('#slide-left').addClass('slide-left-secondary');
+            $('#search-text').addClass('search-transition');
+    };
+});
